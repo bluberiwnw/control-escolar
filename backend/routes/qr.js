93be90cc1e35717ca2c9245513d406c516e8da18
bluberiwnw <garzonsotoraquel@gmail.com>
2026-04-11 03:28:51 -0600

    Sistema Control EScolar PostgreSQL Roles P35

--- backend/routes/qr.js
+++ backend/routes/qr.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const qrController = require('../controllers/qrController');
 const authMiddleware = require('../middleware/authMiddleware');
-const verificarRol = require('../middleware/roleMiddleware');
+const { verificarRol } = require('../middleware/roleMiddleware');
 
 router.post('/generar', authMiddleware, verificarRol(['profesor', 'administrador']), qrController.generarQR);
 router.post('/validar', authMiddleware, verificarRol(['alumno']), qrController.registrarAsistenciaQR);
