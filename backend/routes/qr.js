dcdd1a8278d213dd6f25805bd7a3d8a8d416c6f7
bluberiwnw <garzonsotoraquel@gmail.com>
2026-04-27 17:56:00 -0600

    Sistema Control EScolar PostgreSQL Roles P170

--- backend/routes/qr.js
+++ backend/routes/qr.js
@@ -5,6 +5,6 @@ const authMiddleware = require('../middleware/authMiddleware');
 const { verificarRol } = require('../middleware/roleMiddleware');
 
 router.post('/generar', authMiddleware, verificarRol(['profesor', 'administrador']), qrController.generarQR);
-router.post('/validar', authMiddleware, verificarRol(['alumno']), qrController.registrarAsistenciaQR);
+router.post('/validar', authMiddleware, qrController.registrarAsistenciaQR);
 
 module.exports = router;
